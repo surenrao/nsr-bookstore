1a7e56d306819e9939d9d67bd12ee2043e8997ce
surenrao <surenrao@357b0162-4b02-11de-898f-4722afa3338a>
2009-06-16 02:31:27 +0000

    Added Security package

---
+++ java/bookstore-beans/src/main/java/com/nyayapati/bookstore/model/security/Module.java
@@ -0,0 +1,11 @@
+package com.nyayapati.bookstore.model.security;
+
+import java.util.List;
+
+
+public class Module {
+	private int id;
+	private String name;
+	private int level;
+	private List<SubModule> subModules;
+}
---
+++ java/bookstore-beans/src/main/java/com/nyayapati/bookstore/model/security/SubModule.java
@@ -0,0 +1,7 @@
+package com.nyayapati.bookstore.model.security;
+
+public class SubModule {
+	private Module module;
+	private String name;
+	private int level;
+}
